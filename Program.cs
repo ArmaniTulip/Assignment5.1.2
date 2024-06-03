4e55af742b35c0f1f06be9e069e1863683a9e0c5
Garrett <Garrett@YUP>
2024-06-03 14:56:04 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection.Metadata;
+
+namespace Assignment5._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter numbers for the total sum");
+
+            int userNums = Convert.ToInt32(Console.ReadLine());
+
+            int sum =SumofDigits(userNums);
+
+            Console.WriteLine(sum.ToString());
+
+            
+
+        }
+
+        static int SumofDigits(int number)
+        {
+            int sum = 0;
+            while (number != 0) 
+            {
+                sum += number % 10;
+
+                number /= 10;
+            }
+            return sum;
+        }
+    }
+}
